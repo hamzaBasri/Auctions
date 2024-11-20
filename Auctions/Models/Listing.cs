368efe219e53557d04bd40e241753a657e059eae
Hamza <hamzabasri@OUTLOOK.FR>
2024-11-20 11:42:00 -0500

    Ajoutez des fichiers projet.

---
+++ Auctions/Models/Listing.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Auctions.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsSold { get; set; }
+        public int IdentityUserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
